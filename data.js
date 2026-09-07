27546310d9176e91010220ebcd6bde66f6cb06e3
HuangTao6666 <17815123673@163.com>
2026-09-07 12:02:07 +0800

    publish: data.js

---
+++ data.js
@@ -0,0 +1,743 @@
+// 由 convert_excel.py 自动生成，请勿手工修改
+window.BUILTIN_SCHEDULE = {
+  "settings": {
+    "semesterStart": "2026-09-07",
+    "daysPerWeek": 7,
+    "periodCount": 12
+  },
+  "courses": [
+    {
+      "day": 0,
+      "name": "文学与伦理",
+      "nature": "",
+      "exam": "",
+      "teacher": "超星尔雅",
+      "room": "",
+      "note": "",
+      "noTime": true,
+      "weeks": [
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8
+      ],
+      "weekText": "1-8周",
+      "periods": [],
+      "periodText": ""
+    },
+    {
+      "day": 1,
+      "name": "嵌入式系统人机交互开发实训",
+      "nature": "选修",
+      "exam": "考查",
+      "teacher": "雷仕英",
+      "room": "实验室 7704",
+      "note": "组班：24物联网3班(53)",
+      "noTime": false,
+      "weeks": [
+        14,
+        15,
+        16,
+        17
+      ],
+      "weekText": "14-17周",
+      "periods": [
+        1,
+        2,
+        3,
+        4
+      ],
+      "periodText": "1-4节"
+    },
+    {
+      "day": 1,
+      "name": "嵌入式系统设计与开发",
+      "nature": "选修",
+      "exam": "考查",
+      "teacher": "雷仕英",
+      "room": "实验室 7704",
+      "note": "组班：24物联网3班(53)",
+      "noTime": false,
+      "weeks": [
+        9,
+        10,
+        11,
+        12,
+        13
+      ],
+      "weekText": "9-13周",
+      "periods": [
+        1,
+        2,
+        3,
+        4
+      ],
+      "periodText": "1-4节"
+    },
+    {
+      "day": 1,
+      "name": "物联网工程管理",
+      "nature": "必修",
+      "exam": "考试",
+      "teacher": "夏娟",
+      "room": "智慧教室 6307",
+      "note": "组班：24物联网3班(53)",
+      "noTime": false,
+      "weeks": [
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8
+      ],
+      "weekText": "1-8周",
+      "periods": [
+        1,
+        2
+      ],
+      "periodText": "1-2节"
+    },
+    {
+      "day": 1,
+      "name": "零代码嵌入式系统应用实训",
+      "nature": "选修",
+      "exam": "考查",
+      "teacher": "雷仕英",
+      "room": "实验室 7704",
+      "note": "组班：24物联网3班(53)",
+      "noTime": false,
+      "weeks": [
+        18
+      ],
+      "weekText": "18周",
+      "periods": [
+        1,
+        2,
+        3,
+        4
+      ],
+      "periodText": "1-4节"
+    },
+    {
+      "day": 1,
+      "name": "面向对象程序设计（B）",
+      "nature": "选修",
+      "exam": "考试",
+      "teacher": "宋馨来",
+      "room": "实验室 7704",
+      "note": "组班：24物联网3班(53)",
+      "noTime": false,
+      "weeks": [
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8
+      ],
+      "weekText": "1-8周",
+      "periods": [
+        3,
+        4
+      ],
+      "periodText": "3-4节"
+    },
+    {
+      "day": 2,
+      "name": "PCB设计与应用",
+      "nature": "选修",
+      "exam": "考查",
+      "teacher": "严天华",
+      "room": "实验室 71006",
+      "note": "组班：24物联网3班(53)",
+      "noTime": false,
+      "weeks": [
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8
+      ],
+      "weekText": "1-8周",
+      "periods": [
+        1,
+        2
+      ],
+      "periodText": "1-2节"
+    },
+    {
+      "day": 2,
+      "name": "嵌入式系统人机交互开发实训",
+      "nature": "选修",
+      "exam": "考查",
+      "teacher": "雷仕英",
+      "room": "实验室 7704",
+      "note": "组班：24物联网3班(53)",
+      "noTime": false,
+      "weeks": [
+        14,
+        15,
+        16,
+        17
+      ],
+      "weekText": "14-17周",
+      "periods": [
+        1,
+        2,
+        3,
+        4
+      ],
+      "periodText": "1-4节"
+    },
+    {
+      "day": 2,
+      "name": "嵌入式系统设计与开发",
+      "nature": "选修",
+      "exam": "考查",
+      "teacher": "雷仕英",
+      "room": "实验室 7704",
+      "note": "组班：24物联网3班(53)",
+      "noTime": false,
+      "weeks": [
+        9,
+        10,
+        11,
+        12,
+        13
+      ],
+      "weekText": "9-13周",
+      "periods": [
+        1,
+        2,
+        3,
+        4
+      ],
+      "periodText": "1-4节"
+    },
+    {
+      "day": 2,
+      "name": "零代码嵌入式系统应用实训",
+      "nature": "选修",
+      "exam": "考查",
+      "teacher": "雷仕英",
+      "room": "实验室 7704",
+      "note": "组班：24物联网3班(53)",
+      "noTime": false,
+      "weeks": [
+        18
+      ],
+      "weekText": "18周",
+      "periods": [
+        1,
+        2,
+        3,
+        4
+      ],
+      "periodText": "1-4节"
+    },
+    {
+      "day": 2,
+      "name": "面向对象程序设计（B）",
+      "nature": "选修",
+      "exam": "考试",
+      "teacher": "宋馨来",
+      "room": "实验室 7705",
+      "note": "组班：24物联网3班(53)",
+      "noTime": false,
+      "weeks": [
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8
+      ],
+      "weekText": "1-8周",
+      "periods": [
+        3,
+        4
+      ],
+      "periodText": "3-4节"
+    },
+    {
+      "day": 2,
+      "name": "Linux系统编程",
+      "nature": "选修",
+      "exam": "考试",
+      "teacher": "雷仕英",
+      "room": "实验室 7705",
+      "note": "组班：24物联网3班(53)",
+      "noTime": false,
+      "weeks": [
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8
+      ],
+      "weekText": "1-8周",
+      "periods": [
+        5,
+        6
+      ],
+      "periodText": "5-6节"
+    },
+    {
+      "day": 2,
+      "name": "云计算与边缘计算（B）",
+      "nature": "选修",
+      "exam": "考试",
+      "teacher": "王朝卿",
+      "room": "实验室 7704",
+      "note": "组班：24物联网3班(53)",
+      "noTime": false,
+      "weeks": [
+        9,
+        10,
+        11,
+        12,
+        13,
+        14,
+        15,
+        16
+      ],
+      "weekText": "9-16周",
+      "periods": [
+        9,
+        10
+      ],
+      "periodText": "9-10节"
+    },
+    {
+      "day": 3,
+      "name": "嵌入式系统人机交互开发实训",
+      "nature": "选修",
+      "exam": "考查",
+      "teacher": "雷仕英",
+      "room": "实验室 7704",
+      "note": "组班：24物联网3班(53)",
+      "noTime": false,
+      "weeks": [
+        14,
+        15,
+        16,
+        17
+      ],
+      "weekText": "14-17周",
+      "periods": [
+        1,
+        2,
+        3,
+        4
+      ],
+      "periodText": "1-4节"
+    },
+    {
+      "day": 3,
+      "name": "嵌入式系统设计与开发",
+      "nature": "选修",
+      "exam": "考查",
+      "teacher": "雷仕英",
+      "room": "实验室 7704",
+      "note": "组班：24物联网3班(53)",
+      "noTime": false,
+      "weeks": [
+        9,
+        10,
+        11,
+        12,
+        13
+      ],
+      "weekText": "9-13周",
+      "periods": [
+        1,
+        2,
+        3,
+        4
+      ],
+      "periodText": "1-4节"
+    },
+    {
+      "day": 3,
+      "name": "物联网工程管理",
+      "nature": "必修",
+      "exam": "考试",
+      "teacher": "夏娟",
+      "room": "智慧教室 6308",
+      "note": "组班：24物联网3班(53)",
+      "noTime": false,
+      "weeks": [
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8
+      ],
+      "weekText": "1-8周",
+      "periods": [
+        1,
+        2
+      ],
+      "periodText": "1-2节"
+    },
+    {
+      "day": 3,
+      "name": "零代码嵌入式系统应用实训",
+      "nature": "选修",
+      "exam": "考查",
+      "teacher": "雷仕英",
+      "room": "实验室 7704",
+      "note": "组班：24物联网3班(53)",
+      "noTime": false,
+      "weeks": [
+        18
+      ],
+      "weekText": "18周",
+      "periods": [
+        1,
+        2,
+        3,
+        4
+      ],
+      "periodText": "1-4节"
+    },
+    {
+      "day": 3,
+      "name": "PCB设计与应用",
+      "nature": "选修",
+      "exam": "考查",
+      "teacher": "严天华",
+      "room": "实验室 71006",
+      "note": "组班：24物联网3班(53)",
+      "noTime": false,
+      "weeks": [
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8
+      ],
+      "weekText": "1-8周",
+      "periods": [
+        3,
+        4
+      ],
+      "periodText": "3-4节"
+    },
+    {
+      "day": 4,
+      "name": "嵌入式系统人机交互开发实训",
+      "nature": "选修",
+      "exam": "考查",
+      "teacher": "雷仕英",
+      "room": "实验室 7704",
+      "note": "组班：24物联网3班(53)",
+      "noTime": false,
+      "weeks": [
+        14,
+        15,
+        16,
+        17
+      ],
+      "weekText": "14-17周",
+      "periods": [
+        1,
+        2,
+        3,
+        4
+      ],
+      "periodText": "1-4节"
+    },
+    {
+      "day": 4,
+      "name": "嵌入式系统设计与开发",
+      "nature": "选修",
+      "exam": "考查",
+      "teacher": "雷仕英",
+      "room": "实验室 7704",
+      "note": "组班：24物联网3班(53)",
+      "noTime": false,
+      "weeks": [
+        9,
+        10,
+        11,
+        12,
+        13
+      ],
+      "weekText": "9-13周",
+      "periods": [
+        1,
+        2,
+        3,
+        4
+      ],
+      "periodText": "1-4节"
+    },
+    {
+      "day": 4,
+      "name": "形势与政策（五）",
+      "nature": "必修",
+      "exam": "",
+      "teacher": "陈荣",
+      "room": "研讨互动智慧教室 71202",
+      "note": "组班：24物联网4班(47),24物联网3班(53)",
+      "noTime": false,
+      "weeks": [
+        1,
+        2,
+        3,
+        4
+      ],
+      "weekText": "1-4周",
+      "periods": [
+        1,
+        2
+      ],
+      "periodText": "1-2节"
+    },
+    {
+      "day": 4,
+      "name": "零代码嵌入式系统应用实训",
+      "nature": "选修",
+      "exam": "考查",
+      "teacher": "雷仕英",
+      "room": "实验室 7704",
+      "note": "组班：24物联网3班(53)",
+      "noTime": false,
+      "weeks": [
+        18
+      ],
+      "weekText": "18周",
+      "periods": [
+        1,
+        2,
+        3,
+        4
+      ],
+      "periodText": "1-4节"
+    },
+    {
+      "day": 4,
+      "name": "面向对象程序设计（B）",
+      "nature": "选修",
+      "exam": "考试",
+      "teacher": "宋馨来",
+      "room": "实验室 7705",
+      "note": "组班：24物联网3班(53)",
+      "noTime": false,
+      "weeks": [
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8
+      ],
+      "weekText": "1-8周",
+      "periods": [
+        3,
+        4
+      ],
+      "periodText": "3-4节"
+    },
+    {
+      "day": 4,
+      "name": "面向对象程序设计（B）",
+      "nature": "选修",
+      "exam": "考试",
+      "teacher": "宋馨来",
+      "room": "实验室 7704",
+      "note": "组班：24物联网3班(53)",
+      "noTime": false,
+      "weeks": [
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8
+      ],
+      "weekText": "1-8周",
+      "periods": [
+        7,
+        8
+      ],
+      "periodText": "7-8节"
+    },
+    {
+      "day": 4,
+      "name": "云计算与边缘计算（B）",
+      "nature": "选修",
+      "exam": "考试",
+      "teacher": "王朝卿",
+      "room": "实验室 7704",
+      "note": "组班：24物联网3班(53)",
+      "noTime": false,
+      "weeks": [
+        9,
+        10,
+        11,
+        12,
+        13,
+        14,
+        15,
+        16
+      ],
+      "weekText": "9-16周",
+      "periods": [
+        9,
+        10
+      ],
+      "periodText": "9-10节"
+    },
+    {
+      "day": 5,
+      "name": "Linux系统编程",
+      "nature": "选修",
+      "exam": "考试",
+      "teacher": "雷仕英",
+      "room": "实验室 7704",
+      "note": "组班：24物联网3班(53)",
+      "noTime": false,
+      "weeks": [
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8
+      ],
+      "weekText": "1-8周",
+      "periods": [
+        1,
+        2
+      ],
+      "periodText": "1-2节"
+    },
+    {
+      "day": 5,
+      "name": "嵌入式系统人机交互开发实训",
+      "nature": "选修",
+      "exam": "考查",
+      "teacher": "雷仕英",
+      "room": "实验室 7704",
+      "note": "组班：24物联网3班(53)",
+      "noTime": false,
+      "weeks": [
+        14,
+        15,
+        16,
+        17
+      ],
+      "weekText": "14-17周",
+      "periods": [
+        1,
+        2,
+        3,
+        4
+      ],
+      "periodText": "1-4节"
+    },
+    {
+      "day": 5,
+      "name": "嵌入式系统设计与开发",
+      "nature": "选修",
+      "exam": "考查",
+      "teacher": "雷仕英",
+      "room": "实验室 7704",
+      "note": "组班：24物联网3班(53)",
+      "noTime": false,
+      "weeks": [
+        9,
+        10,
+        11,
+        12,
+        13
+      ],
+      "weekText": "9-13周",
+      "periods": [
+        1,
+        2,
+        3,
+        4
+      ],
+      "periodText": "1-4节"
+    },
+    {
+      "day": 5,
+      "name": "零代码嵌入式系统应用实训",
+      "nature": "选修",
+      "exam": "考查",
+      "teacher": "雷仕英",
+      "room": "实验室 7704",
+      "note": "组班：24物联网3班(53)",
+      "noTime": false,
+      "weeks": [
+        18
+      ],
+      "weekText": "18周",
+      "periods": [
+        1,
+        2,
+        3,
+        4
+      ],
+      "periodText": "1-4节"
+    },
+    {
+      "day": 5,
+      "name": "PCB设计与应用",
+      "nature": "选修",
+      "exam": "考查",
+      "teacher": "严天华",
+      "room": "实验室 71006",
+      "note": "组班：24物联网3班(53)",
+      "noTime": false,
+      "weeks": [
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8
+      ],
+      "weekText": "1-8周",
+      "periods": [
+        3,
+        4
+      ],
+      "periodText": "3-4节"
+    }
+  ]
+};
